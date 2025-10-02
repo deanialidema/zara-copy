4c934b55074fd1b242126df964bff0085a03f6c9
Zk <momqillamerita3@gmail.com>
2025-10-02 02:53:58 +0100

    Update route.ts

--- app/api/verify-recaptcha/route.ts
+++ app/api/verify-recaptcha/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY || "6LdML9grAAAAADJSZYcSVDYmQAKvkTvbcOQifpb9"
+const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY || "6LcslNsrAAAAAPGl9iDJtd3cRyQfb_AhYN90Yb9Z"
 
 export async function POST(request: NextRequest) {
   try {
