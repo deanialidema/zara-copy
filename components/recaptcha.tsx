728310609a5596e7a21d7fd338e228803f967661
Zk <momqillamerita3@gmail.com>
2025-10-13 15:40:23 +0100

    Update recaptcha.tsx

--- components/recaptcha.tsx
+++ components/recaptcha.tsx
@@ -25,7 +25,7 @@ interface RecaptchaProps {
   theme?: 'light' | 'dark'
 }
 
-const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LdkQ90rAAAAAJI1U_1IqI_1qjW2suWo9KQNNAKB"
+const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6Ld42egrAAAAAGn7KlYfvwYT6IhQ2JD8O9ft3SJ4"
 
 // Hook to check if reCAPTCHA is loaded
 function useRecaptchaLoaded() {
